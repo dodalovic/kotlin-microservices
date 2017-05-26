0dbd8107ae6cf3ac2de513390b9405658b1dbda4
Dusan Odalovic <dusan.odalovic@whitewall.com>
2017-05-26 14:09:43 +0200

    Remove braces when no body

--- kotlin-user-service/src/main/kotlin/rs/dodalovic/users/persistence/UserRepository.kt
+++ kotlin-user-service/src/main/kotlin/rs/dodalovic/users/persistence/UserRepository.kt
@@ -3,5 +3,4 @@ package rs.dodalovic.users.persistence
 import org.springframework.data.repository.CrudRepository
 import rs.dodalovic.users.model.User
 
-interface UserRepository : CrudRepository<User, String> {
-}
+interface UserRepository : CrudRepository<User, String>
